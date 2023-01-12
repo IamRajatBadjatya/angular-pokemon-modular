16bb4378a93e574af8a5cc36ae28ce00d43171b3
IamRajatBadjatya <31719594+IamRajatBadjatya@users.noreply.github.com>
2023-01-13 00:35:22 +0100

    prepare demo branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable } from 'rxjs';
-
-import { Pokemon } from './pokemon.model';
-import { PokemonService } from './pokemon.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  pokemons$: Observable<Pokemon[]> = this.pokemonService.getPokemons();
-  constructor(private readonly pokemonService: PokemonService) {}
-}
+export class AppComponent {}
